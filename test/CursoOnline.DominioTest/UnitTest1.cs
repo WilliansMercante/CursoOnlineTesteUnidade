53b24e805357404d2250bb7b1b62af7ff982d5c8
WilliansMercante <willians_mercante@hotmail.com>
2023-11-29 22:00:58 -0300

    Adicionar arquivos de projeto.

---
+++ test/CursoOnline.DominioTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CursoOnline.DominioTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ test/CursoOnline.DominioTest/_Utils/AssertExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace CursoOnline.DominioTest._Utils
+{
+    internal static class AssertExtensions
+    {
+        public static void ComMensagem(this ArgumentException exception,string mensagem)
+        {
+            if (exception.Message == mensagem) 
+            {
+                Assert.True(true);
+            }
+            else
+            {
+                Assert.False(true,"Esperava a mensagem: **" + mensagem + "**, porém veio a mensagem: **" + exception.Message + "**");
+            }
+        }
+    }
+}
